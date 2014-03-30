3d3c550ccc92e899e6034025ecb0a18d1afc946b
Marin Usalj <mneorr@gmail.com>
2014-03-30 13:38:46 -0700

    implemented some watching

--- lib/command/watch.rb
+++ lib/command/watch.rb
@@ -1,18 +1,48 @@
+require 'listen'
+
 module Pod
   class Command
     class Watch < Command
+
       self.summary = 'Watches Podfile for changes and runs `pod install`'
 
-      #def initialize(argv)
-        #@name = argv.shift_argument
-        #super
-      #end
+      def run
+        listener = Listen.to(Dir.pwd, :ignore => ignored_paths, &listen_action)
+        listener.start
+        sleep
+      end
 
-      #def run
-        #puts "IMPLEMENT ME"
-      #end
+      def ignored_paths
+        [/.git/,
+         /.xc(odeproj|workspace|userdata|scheme|config)/,
+         /.lock$/,
+         /\.txt$/,
+         /\.log$/]
+      end
+
+      def listen_action
+        lambda do |modified, added, removed|
+          if modified.first =~ /Podfile$/
+            install_pods
+          end
+        end
+      end
+
+      private
+
+      def install_pods
+        ShellTask.run('pod install')
+      end
 
     end
+
+    class ShellTask
+      def self.run(args)
+        puts args
+        fork { exec args }
+      end
+    end
+
   end
 end
 
--- spec/command/watch_spec.rb
+++ spec/command/watch_spec.rb
@@ -3,16 +3,60 @@ require File.expand_path('../../spec_helper', __FILE__)
 # The CocoaPods namespace
 #
 module Pod
-  describe Command::Watch do
 
-    #-------------------------------------------------------------------------#
+  class Command < CLAide::Command
 
-    describe "Try" do
-      it "registers it self" do
-        Command.parse(%w(watch)).should.be.instance_of Command::Watch
-      end
+    def sleep
+      "CAFEINEEEE NO SLEEP"
     end
 
+    describe Watch do
+
+      #class ShellTask
+        #def self.run(args)
+          #@@command = args
+        #end
+      #end
+
+      before do
+        @base_dir = Dir.pwd
+        @command = Command.parse(%w(watch))
+        @listen_event = @command.listen_action
+      end
+
+      it "registers itself as a command" do
+        @command.should.be.instance_of Command::Watch
+      end
+
+      it 'ignores everything except objc files' do
+        @command.ignored_paths.should == [
+          /.git/,
+          /.xc(odeproj|workspace|userdata|scheme|config)/,
+          /.lock$/,
+          /\.txt$/,
+          /\.log$/
+        ]
+      end
+
+      it 'listens to file changes' do
+        mock_listener = stub
+        Listen.expects(:to)
+          .with(Dir.pwd, :ignore => @command.ignored_paths)
+          .returns(mock_listener)
+        mock_listener.expects(:start)
+        @command.run
+      end
+
+      it 'reinstalls pods once the podfile is changed' do
+        ShellTask.expects(:run).with('pod install')
+        @listen_event.call(['Podfile'], [], [])
+      end
+
+      #it 'watches for additions/deletions in local pods' do
+
+      #end
+
+    end
   end
 end
 
